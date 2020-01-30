7e636ab92b9113ed1ed487746a7ee26ffe4d77ee
AlharranAbeer <AlharranAbeer@gmail.com>
2020-01-30 23:01:57 +0300

    Enter

--- App.js
+++ App.js
@@ -4,13 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
       <Text>oyoyoyoyoyoy</Text>
       <Text>yuyuyuyuy</Text>
-=======
-      <Text>hi everyone</Text>
-      <Text>we will be fine line !</Text>
->>>>>>> edf46ffae30f665882c51d4ef91ae2f454b2753d
     </View>
   );
 }
