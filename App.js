5e98a3c9c56ac94fc099c5fda3ea93a97618015b
AlharranAbeer <AlharranAbeer@gmail.com>
2020-01-31 17:43:32 +0300

    commit

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "<YOUR-API-KEY>",
+  authDomain: "<YOUR-AUTH-DOMAIN>",
+  databaseURL: "<YOUR-DATABASE-URL>",
+  storageBucket: "<YOUR-STORAGE-BUCKET>"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
